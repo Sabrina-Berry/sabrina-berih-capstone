0e183780d20e30999b7a680f30a9a6eb41eef4ca
Sabrina B <sabrinaberih@gmail.com>
2024-10-20 15:01:45 -0400

    complete header for all screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import HomePage from "./pages/HomePage/HomePage";
 import Header from "./components/Header/Header";
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,46 @@
+import "./Header.scss";
+import logo from "../../assets/logo/logo.png";
+import { useLocation, Link } from "react-router-dom";
+
 function Header() {
+  const { pathname } = useLocation();
+  const splitLocation = pathname.split("/");
+
   return (
-    <div className="header">
-      <p>I'm the header</p>
-    </div>
+    <header className="header">
+      <div className="header__wrapper">
+        <Link to="/" className="header__logo-link">
+          <img
+            className="header__logo-img"
+            src={logo}
+            alt="I Want to Help Logo"
+          />
+          <p className="header__logo-text"> I Want to Help</p>
+        </Link>
+        <ul className="header__nav">
+          <li
+            className={`header__nav-item ${
+              splitLocation[2] === "HomePage" || splitLocation[1] === ""
+                ? "header__nav-item--active"
+                : ""
+            }`}
+          >
+            <Link to="/" className="header__nav-link">
+              Home
+            </Link>
+          </li>
+          <li
+            className={`header__nav-item ${
+              splitLocation[1] === "AboutPage" ? "header__nav-item--active" : ""
+            }`}
+          >
+            <Link to="/AboutPage" className="header__nav-link">
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 }
 
