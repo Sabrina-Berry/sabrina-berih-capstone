4956252f2b15f4c3fa43ba97c338ff3f3efd4b51
Sabrina B <sabrinaberih@gmail.com>
2024-10-19 20:38:36 -0400

    Add basic files for testing

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="header">
+      <p>I'm the header</p>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div className="home-page">
+      <p>I'm the home page</p>
+    </div>
+  );
+}
+
+export default HomePage;
